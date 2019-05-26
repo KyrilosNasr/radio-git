3cb3b41a2b4f4796f6bbb74399bf1ba96a1afdbb
abanoub essam <20130130@fa-hists.edu.eg>
2019-05-26 23:32:46 +0200

    firebase initialized
    at env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB6EAXvoJuAgE89jftUCRZAqKtkVGCSQtE",
+    authDomain: "radiodawod-e369e.firebaseapp.com",
+    databaseURL: "https://radiodawod-e369e.firebaseio.com",
+    projectId: "radiodawod-e369e",
+    storageBucket: "radiodawod-e369e.appspot.com",
+    messagingSenderId: "1066299780499",
+    appId: "1:1066299780499:web:0c077b1be88ea20d"
+  }
 };
 
 /*
